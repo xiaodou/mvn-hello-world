6b42b03ebfe8b5c730ca26f0dcc6f5a27dcb4f83
xiaodou <douguangpeng0910@163.com>
2020-05-27 23:30:48 +0800

    测试

---
+++ src/main/java/test/controller/TestController.java
@@ -0,0 +1,12 @@
+package test.controller;
+
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2020/5/27 0027.
+ */
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+    }
+}
